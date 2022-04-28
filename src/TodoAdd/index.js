b0b64e78e62759c1aab05be36b499c1a26bbc30e
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-04-28 09:50:56 +0900

    Update index.js

--- src/TodoAdd/index.js
+++ src/TodoAdd/index.js
@@ -1,8 +1,17 @@
 import styled from "styled-components";
 
+const Textbox=styled.div`
+background-color:red;
+display:block;`
+const Add=styled.button`
+background-color:blue;
+display:block`
 function B(){
     return(
-        <div></div>
+        <div>
+            <Textbox>Finish</Textbox>
+            <Add>Add</Add>
+        </div>
     )
 }
 export default B
