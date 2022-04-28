0681002ff04ad6b94d167348e4c2f4523f9619bc
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-04-28 09:52:35 +0900

    Update index1.js

--- src/Tododate/index1.js
+++ src/Tododate/index1.js
@@ -2,10 +2,17 @@ import styled from "styled-components";
 import React from "react";
 
 function A(){
+    const today=new Date();
+    console.log(today.getFullYear());
+    console.log(today.getMonth())
+    console.log(today.getDate())
     return(
     <div>
-       
-    </div>    
+       {today.getFullYear()}.{today.getMonth()}.{today.getDate()}
+    </div>
+    
+    
+        
     )
 }
 
