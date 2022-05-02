5a9a2d0fc4a3f0a79184559d2d59c17a090fc082
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-05-02 09:27:53 +0900

    Update index1.js

--- src/Tododate/index1.js
+++ src/Tododate/index1.js
@@ -6,9 +6,11 @@ function A(){
     console.log(today.getFullYear());
     console.log(today.getMonth()+1)
     console.log(today.getDate())
+    const today1=today.toLocaleDateString("en-US",{weekday:"long"});
     return(
     <div>
        {today.getFullYear()}.{today.getMonth()+1}.{today.getDate()}
+       <today1>{today1}</today1>
     </div>
     
     
