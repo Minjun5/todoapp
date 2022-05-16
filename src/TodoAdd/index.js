559737c4e4fee2848315c71b1a3a7a07c4d8d80f
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-05-16 20:34:46 +0900

    Update index.js

--- src/TodoAdd/index.js
+++ src/TodoAdd/index.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import styled from "styled-components";
+import e from "../todoitem/index3";
 
 const Textbox=styled.div`
 background-color:red;
@@ -6,12 +8,34 @@ display:block;`
 const Add=styled.button`
 background-color:blue;
 display:block`
+function onclick(){
+    alert("ddd")
+}
+function todoadd({todos, updateTodo}){
+    const[todo, seTtodo]=useState({
+        text:""
+    })
+    const addTodo=(e)=>{
+    const {name,value}=e.target;
+    console.log(name)
+    console.log(value)
+    setuser({...user,
+    [name]:value}
+    );console.log(user)
+    }
+    }
+}
 function B(){
+    
     return(
+        
         <div>
             <Textbox>Finish</Textbox>
-            <Add>Add</Add>
+            <Add onClick={onclick}>Add</Add>
+            
+            )
         </div>
     )
 }
+
 export default B
