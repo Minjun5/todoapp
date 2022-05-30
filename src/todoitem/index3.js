eaac1ced053ebdc2d8066825049d08285094c32a
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-05-30 09:54:13 +0900

    Update index3.js

--- src/todoitem/index3.js
+++ src/todoitem/index3.js
@@ -11,16 +11,32 @@ height:30px;
 
 
 
-function e() {
-    const [todos, setTodos]=useState([{isDone:null, text:"",id:null}])
-    const removeTodo=(todo)=>{
-        setTodos([...null])
+function e({todos, updateTodo}){
+    const[todo, setTodo]=useState({
+        id:null,
+        text:"",
+        isDone:false,
+    })
+    
+    const addTodo=(e)=>{
+    const {name,value}=e.target;
+        setTodo({
+            id: todos.length,
+            isDone:false,
+            text: value
+        })
+
     }
+
+
+const onCLickpage=()=>{
+    updateTodo(todo)
+}
     return(
         
         <Container>
             
-        <Img src="https://cdn2.vectorstock.com/i/1000x1000/01/71/trash-can-icon-vector-13490171.jpg" todos={todos} updateTodo={removeTodo}/>
+        <Img src="https://cdn2.vectorstock.com/i/1000x1000/01/71/trash-can-icon-vector-13490171.jpg" todos={todos} onChange={addTodo} onClick={onCLickpage}/>
         </Container>
         
     )
