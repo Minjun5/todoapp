f3696cf985c1fbf11adc0400500f52a790ae8773
Minjun5 <89818248+Minjun5@users.noreply.github.com>
2022-06-01 18:24:49 +0900

    Update index.js

--- src/TodoAdd/index.js
+++ src/TodoAdd/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
-import e from "../todoitem/index3";
+import React from "react";
+import { text } from "dom-helpers";
 
 const Textbox=styled.div`
 background-color:red;
@@ -8,34 +9,38 @@ display:block;`
 const Add=styled.button`
 background-color:blue;
 display:block`
-function onclick(){
-    alert("ddd")
-}
-function todoadd({todos, updateTodo}){
-    const[todo, seTtodo]=useState({
-        text:""
+const Input=styled.input``
+
+function TodoAdd({todos, updateTodo}){
+    const[todo, setTodo]=useState({
+        id:null,
+        text:"",
+        isDone:false,
     })
+    
     const addTodo=(e)=>{
     const {name,value}=e.target;
-    console.log(name)
-    console.log(value)
-    setuser({...user,
-    [name]:value}
-    );console.log(user)
+        setTodo({
+            id: todos.length,
+            isDone:false,
+            text: value
+        })
+
     }
-    }
+
+
+const onCLickpage=()=>{
+    updateTodo(todo)
 }
-function B(){
-    
-    return(
-        
+return(
         <div>
             <Textbox>Finish</Textbox>
-            <Add onClick={onclick}>Add</Add>
+            <Add onClick={onCLickpage}>Add</Add>
+            <Input type="text" name="ID" value={todo.text} onChange={addTodo}></Input>
             
-            )
         </div>
-    )
-}
+    )}
 
-export default B
+
+
+export default TodoAdd
